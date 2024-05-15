e063b2b375bde5f0bbed26b9953aeb5740d6cb5f
Jorge Gutierrez <jorgeegutierrez56615@gmail.com>
2024-05-15 03:21:10 -0700

    Update Animals 10-JG.py

--- Animals 10-JG.py
+++ Animals 10-JG.py
@@ -10,4 +10,38 @@ data_dir = 'raw-img-skewed'
 data_dir = pathlib.Path(data_dir).with_suffix('')
 
 image_count = len(list(data_dir.glob('*/*.jpg')))
+print()
 print(image_count)
+print()
+
+img_height = 250
+img_width = 250
+batch_size = 64
+epochs = 10
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split = 0.2,
+    subset='training',
+    seed = 123,
+    image_size= (img_height,img_width),
+    batch_size=batch_size
+)
+print()
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.3,
+    seed=123,
+    subset='validation',
+    image_size=(img_height, img_width),
+    batch_size = batch_size
+)
+print()
+
+normalization_layer = layers.Rescaling(1./255)
+normalized_ds  = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+
+print(np.min(first_image), np.max(first_image))
