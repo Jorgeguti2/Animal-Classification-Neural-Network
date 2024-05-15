a0a30f2d3dd59d669f9bfa1412a1bb1458c33a66
Jorge Gutierrez <jorgeegutierrez56615@gmail.com>
2024-05-14 23:50:08 -0700

    Update Animals 10-JG.py

--- Animals 10-JG.py
+++ Animals 10-JG.py
@@ -15,6 +15,20 @@ class_names = sorted(os.listdir(data_path))
 # Counting the number of classes
 num_classes = len(class_names)
 # Printing the class names and the total number of classes
-print("Class Names: \n", class_names)
+print()
 print("Number of Classes:", num_classes)
+print()
+print("Class Names: \n", class_names)
+print()
+# Geting the number of samples in each class (number of images per class)
+class_sizes = []
+for name in class_names:
+    class_size = len(os.listdir(data_path + "/" + name))
+    class_sizes.append(class_size)
+# Printing the class distribution
+print("Class Distribution:\n", class_sizes)
+print()
+#  converting lists to dictionary
+class_name_size = dict(zip(class_names, class_sizes))
+
 
