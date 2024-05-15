bc1d57d08f305a3b468f66c78dec32c47ebb881e
Jorge Gutierrez <jorgeegutierrez56615@gmail.com>
2024-05-14 23:36:53 -0700

    Update Animals 10-JG.py

--- Animals 10-JG.py
+++ Animals 10-JG.py
@@ -1,4 +1,4 @@
-# Setup
+# Import necassary libraries
 import tensorflow as tf
 from tensorflow import keras
 import sklearn
@@ -6,10 +6,15 @@ import numpy as np
 np.random.seed(42)
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import os  # used for interacting with the file system
 
-#Load Dataset
-animals = 
-(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
-X_train, X_valid = X_train_full[:-5000], X_train_full[-5000:]
-y_train, y_valid = y_train_full[:-5000], y_train_full[-5000:]
-X_test = X_test
+# giving path to the animals10 image dataset from kaggle 
+data_path = 'raw-image'
+# Geting a list of all unique class names from the dataset path
+class_names = sorted(os.listdir(data_path))
+# Counting the number of classes
+num_classes = len(class_names)
+# Printing the class names and the total number of classes
+print("Class Names: \n", class_names)
+print("Number of Classes:", num_classes)
+
