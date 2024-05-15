f4020939467e0b79f9d3ea4ab44e69ac3a0ab105
Jorge Gutierrez <jorgeegutierrez56615@gmail.com>
2024-05-15 00:01:55 -0700

    Update Animals 10-JG.py

--- Animals 10-JG.py
+++ Animals 10-JG.py
@@ -10,9 +10,9 @@ import os
 
 # Loading the Animal Dataset
 # giving path to the animals10 image dataset from kaggle 
-data_path = 'raw-img-skewed'
+skewed_data_path = 'raw-img-skewed'
 # Geting a list of all unique class names from the dataset path
-class_names = sorted(os.listdir(data_path))
+class_names = sorted(os.listdir(skewed_data_path))
 # Counting the number of classes
 num_classes = len(class_names)
 # Printing the class names and the total number of classes
@@ -24,7 +24,7 @@ print()
 # Geting the number of samples in each class (number of images per class)
 class_sizes = []
 for name in class_names:
-    class_size = len(os.listdir(data_path + "/" + name))
+    class_size = len(os.listdir(skewed_data_path + "/" + name))
     class_sizes.append(class_size)
 # Printing the class distribution
 print("Class Distribution:\n", class_sizes)
@@ -33,5 +33,7 @@ print()
 class_name_size = dict(zip(class_names, class_sizes))
 
 # Handling Data Imbalance
-# Seting the path to the directory where the sampled data will be saved
-sampled_data_path = r'/kaggle/working/sample-data'
+# Seting the path to the directory where the sample balanced data will be saved
+balanced_data_path = 'raw-img-balanced'
+# Seting the percentage of each class to sample
+sample_percent = 0.1
